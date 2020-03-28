dc367154dd18e77b657de18144feb3aaf0e6b25e
Farani Lucero <farani.lucero@sentar.com>
2020-03-28 16:31:30 -0700

    router

--- router.js
+++ router.js
@@ -2,12 +2,17 @@ const path = require("path");
 const router = require("express").Router();
 
 // If no API routes are hit, send the React app
-router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "test.html"))
+
+router.use("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "login.html"))
 );
 
 router.use("/success", (req, res) =>
   res.sendFile(path.join(__dirname, "success.html"))
+);
+
+router.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "index.html"))
 );
 
 module.exports = router;
