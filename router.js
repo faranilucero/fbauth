c0458fed7bd40bd128ba5e98be278ee3e726a35b
Farani Lucero <faranilucero@gmail.com>
2020-03-30 10:06:42 -0700

    updates

--- router.js
+++ router.js
@@ -3,9 +3,6 @@ const router = require("express").Router();
 
 // If no API routes are hit, send the React app
 
-router.use("/login", (req, res) =>
-  res.sendFile(path.join(__dirname, "login.html"))
-);
 
 router.use("/success", (req, res) =>
   res.sendFile(path.join(__dirname, "success.html"))
